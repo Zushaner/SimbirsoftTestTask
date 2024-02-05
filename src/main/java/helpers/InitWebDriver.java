0ae16778863dcc7d46411fa8a2435f532a9cb2ab
Zushaner <aadlier15@gmail.com>
2024-02-05 15:46:56 +0400

    Исправление комментария

--- src/main/java/helpers/InitWebDriver.java
+++ src/main/java/helpers/InitWebDriver.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class InitWebDriver {
     public static WebDriver initWebDriver() {
         ChromeOptions options = new ChromeOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.EAGER); //использован Eager, так как весе необхадимое загружается, при normal долгая загрузка
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER); //использован Eager, так как все необходимое загружается, при normal долгая загрузка
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().setSize(new Dimension(700,900)); //данное разрешение примененно, так как при изначальном (1440*900) кнопка отпраки перекрыта футером и некликабельна
